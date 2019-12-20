871b3e91d8137daa2d2c15a03f61f6ab470493d9
yourally69@gmail.com <jbkhome69>
2019-12-20 16:31:15 +0300

    feature(frontend): header, http-interceptor, profile

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  get greetingWords(): string {
+    return 'Привет, ';
+  }
   constructor() { }
 
   ngOnInit() {
