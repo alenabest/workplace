f0c4f41e7efba785dd61555060d32ed58ac8260e
ally <yourally69@gmail.com>
2019-12-25 18:59:34 +0300

    feature(frontend): dayjs + приветствие

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
+
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   get greetingWords(): string {
-    return 'Привет, ';
+    const nowHour: number = dayjs().hour();
+    if (nowHour >= 5 && nowHour < 10) {
+      return 'Доброе утро, ';
+    } else if (nowHour >= 10 && nowHour < 18) {
+      return 'Добрый день, ';
+    } else if (nowHour >= 18 && nowHour < 23) {
+      return 'Добрый вечер, ';
+    } else if (nowHour >= 23 && nowHour < 5) {
+      return 'Доброй ночи, ';
+    }
   }
+
   constructor() { }
 
   ngOnInit() {
