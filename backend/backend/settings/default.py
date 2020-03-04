572ca2ff49a10ba59d7e0708f50402ab3cb6c2da
yourally69@gmail.com <jbkhome69>
2020-03-04 11:19:10 +0300

    feature(backend): backend.settings

--- backend/backend/settings/default.py
+++ backend/backend/settings/default.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6^qbm)-s$=vwv!pjya3qgxgzsdd%sxsnjb$uhq385yt6zp=#0*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
